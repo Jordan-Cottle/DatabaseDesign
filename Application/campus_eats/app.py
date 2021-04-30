6a0cbb1cf55619c672677d59758f7db91b53cb37
Jordan Cottle <jordancottle622@gmail.com>
2021-04-30 17:56:54 -0400

    Add admin route

--- Application/campus_eats/app.py
+++ Application/campus_eats/app.py
@@ -25,5 +25,37 @@ def index():
     return render_template("index.html")
 
 
+AVG_DRIVER_COLS = [
+    "driver_id",
+    "name",
+    "avg_speed_rating",
+    "avg_friendliness_rating",
+    "num_ratings"
+]
+
+AVG_RESTAURANT_COLS = [
+    "restaurant_id",
+    "name",
+    "avg_value_rating",
+    "avg_quality_rating",
+    "num_ratings"
+]
+
+@app.route("/admin")
+def admin_view():
+    """ Query aggregated data from database. """
+    driver_data = g.session.execute("select * from avg_driver_ratings").fetchall()
+    drivers = [
+        dict(zip(AVG_DRIVER_COLS, driver)) for driver in driver_data
+    ]
+
+    restaurant_data = g.session.execute("select * from avg_restaurant_ratings").fetchall()
+    restaurants = [
+        dict(zip(AVG_RESTAURANT_COLS, restaurant)) for restaurant in restaurant_data
+    ]
+
+    return render_template("admin.html", drivers=drivers, restaurants=restaurants)
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="3000")
